cc0e254193f56b470fc00f8b7237f1a91777128c
kaiatc <kaiatc@qq.com>
2021-02-24 15:29:35 +0800

    test send http cookie

--- CookieHTTPDemo/main.cpp
+++ CookieHTTPDemo/main.cpp
@@ -1,8 +1,14 @@
 #include "widget.h"
 #include <QApplication>
+#include <QTextCodec>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
+    QTextCodec *utf8TexCodec = QTextCodec::codecForName("UTF-8");
+        qDebug() << "lookKai change textCodec to " << utf8TexCodec->name() << utf8TexCodec->mibEnum();
+        QTextCodec::setCodecForLocale(utf8TexCodec);
+
     QApplication a(argc, argv);
     Widget w;
     w.show();
--- CookieHTTPDemo/widget.cpp
+++ CookieHTTPDemo/widget.cpp
@@ -3,6 +3,7 @@
 #include <QDebug>
 #include <QTcpServer>
 #include <QTcpSocket>
+#include <Windows.h>
 
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
@@ -11,7 +12,7 @@ Widget::Widget(QWidget *parent) :
     ui->setupUi(this);
 
     m_tcpServer = new QTcpServer(this);
-    m_tcpServer->listen(QHostAddress::Any, 80);
+    m_tcpServer->listen(QHostAddress::Any, 8090);
     connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(btnClicked()));
     connect(m_tcpServer, SIGNAL(newConnection()), this, SLOT(newConnectionSlot()));
     connect(m_tcpServer, SIGNAL(acceptError(QAbstractSocket::SocketError)), this, SLOT(errorStringSlot()));
@@ -39,25 +40,40 @@ int choose = 0;
 
 void Widget::btnClicked()
 {
+    QString testChinese = "abc中文1234";
+    testChinese.append(QString::fromWCharArray(L"凯凯\u4e2d\u6587"));
+    qDebug() << "testChinese1:" << testChinese;
+//    MessageBoxW(0, testChinese.toStdWString().c_str(), L"abcd好caption", MB_OK);
     if(choose == 0){
 
-        ui->pushButton->setText("无Cookie");
+        ui->pushButton->setText("no Cookie");
         choose++;
     }
     else if(choose == 1){
 
-        ui->pushButton->setText("domin相关");
+        ui->pushButton->setText("domin");
+        choose++;
+    }
+    else if(choose == 2){
+
+        ui->pushButton->setText("httpOnly");
+        choose++;
+    }
+    else if(choose == 3){
+
+        ui->pushButton->setText("redirect");
         choose++;
     }
     else{
 
-        ui->pushButton->setText("有max-age");
+        ui->pushButton->setText("have max-age");
         choose = 0;
     }
 }
 
 void Widget::sendMsg()
 {
+
     QString contentStr;
     QString str;
     contentStr = "<html>"
@@ -67,14 +83,17 @@ void Widget::sendMsg()
                  "</title>"
                  "</head>"
                  "<body>"
-                 "<h1>Socket 80</h1>"
+                 "<h1>Socket 80</h1>";
+    contentStr += QString::fromWCharArray(L"chinese中文123");
+    contentStr += "<h1 id='id_cookie'></h1>"
                  "</body>"
                  "<script>"
-                 "  console.log(document.cookie)"
+                 "  document.getElementById('id_cookie').innerHTML=document.cookie"
                  "</script>"
                  "</html>";
+    qDebug() << contentStr;
 
-    if(ui->pushButton->text() == "有max-age"){
+    if(ui->pushButton->text() == "have max-age"){
         
         str = "HTTP/1.1 200 OK\r\n";
         str.append("Server:nginx\r\n");
@@ -85,12 +104,33 @@ void Widget::sendMsg()
         str.append(QString("Content-Length:%1\r\n\r\n").arg(contentStr.size()));
         str.append(contentStr);
     }
-    else if(ui->pushButton->text() == "无Cookie"){
+    else if(ui->pushButton->text() == "no Cookie"){
 
         str = "HTTP/1.1 200 OK\r\n";
         str.append("Server:nginx\r\n");
         str.append("Content-Type:text/html\r\n");
         str.append("Access-Control-Allow-Origin: *\r\n");
+        str.append(QString("Content-Length:%1\r\n\r\n").arg(contentStr.size()));
+        str.append(contentStr);
+    }
+    else if(ui->pushButton->text() == "httpOnly"){
+
+        str = "HTTP/1.1 200 OK\r\n";
+        str.append("Server:nginx\r\n");
+        str.append("Content-Type:text/html\r\n");
+        str.append("Set-Cookie: keyHttpOnly=valueHttpOnly;HttpOnly\r\n");
+        str.append("Set-Cookie: keyNoHttpOnly=valueNoHttpOnly\r\n");
+        str.append("Access-Control-Allow-Origin: *\r\n");
+        str.append(QString("Content-Length:%1\r\n\r\n").arg(contentStr.size()));
+        str.append(contentStr);
+    } else if(ui->pushButton->text() == "redirect") {
+        str = "HTTP/1.1 302 Moved Temporarily\r\n";
+        str.append("Server:nginx\r\n");
+        str.append("Content-Type:text/html\r\n");
+        str.append("Set-Cookie: keyHttpOnly=valueHttpOnly;HttpOnly\r\n");
+        str.append("Set-Cookie: keyNoHttpOnly=valueNoHttpOnly\r\n");
+        str.append("Access-Control-Allow-Origin: *\r\n");
+        str.append(QString::fromWCharArray(L"Location: http://weibo.com/?testKey=中文\r\n"));
         str.append(QString("Content-Length:%1\r\n\r\n").arg(contentStr.size()));
         str.append(contentStr);
     }
@@ -106,5 +146,5 @@ void Widget::sendMsg()
         str.append(contentStr);
     }
     
-    m_tcpSocket->write(str.toStdString().c_str());
+    m_tcpSocket->write(str.toUtf8());
 }
